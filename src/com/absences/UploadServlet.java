8ed076a28c894767dc5bcf7d32545f63949a93f7
Nikos Zigopis <nzigopis@gmail.com>
2013-09-29 16:31:37 +0300

    Upladed file to blobstore and used CSV to parse - Greek text not working yet

--- src/com/absences/UploadServlet.java
+++ src/com/absences/UploadServlet.java
@@ -1,7 +1,8 @@
 package com.absences;
 
 import java.io.IOException;
-import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.Map;
 
@@ -10,8 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.appengine.api.blobstore.BlobInfo;
+import au.com.bytecode.opencsv.CSVReader;
+
 import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreInputStream;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 
@@ -24,14 +27,33 @@ public class UploadServlet extends HttpServlet {
         throws ServletException, IOException {
 
         Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(req);
-        List<BlobKey> bkey = blobs.get("myFile");
+        BlobKey bkey = blobs.get("myFile").get(0);
         if (bkey == null) 
         {
             res.sendRedirect("/");
         } 
         else 
         {
-            
+        	BlobstoreInputStream is = new BlobstoreInputStream(bkey);
+        	CSVReader csvReader = new CSVReader(new InputStreamReader(is),';');
+        	List<String[]> rows = csvReader.readAll();
+        	csvReader.close();
+        	res.setContentType("text/html");
+        	PrintWriter w = res.getWriter();
+        	w.println("<html><body><table>");
+        	for (String[] row : rows)
+        	{
+        		w.println("<tr>");
+        		for (String td : row)
+        		{
+        			w.println("<td>");
+            		w.println(td);
+            		w.println("</td>");	
+        		}
+        		w.println("</tr>");
+        	}
+        	w.println("</table></body></html>");
+        	w.close();
         }
     }
 }
--- war/Upload.jsp
+++ war/Upload.jsp
@@ -1,5 +1,5 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 
 <%@ page import="com.google.appengine.api.blobstore.BlobstoreServiceFactory" %>
@@ -15,8 +15,8 @@
     </head>
     <body>
         <form action="<%= blobstoreService.createUploadUrl("/upload") %>" method="post" enctype="multipart/form-data">
-            <input type="text" name="foo">
-            <input type="file" name="myFile">
+            <label for="myFile">Επιλέξτε αρχείο τύπου .csv</label>
+            <input id="myFile" type="file" name="myFile">
             <input type="submit" value="Submit">
         </form>
     </body>
