f914f4ef53c6b0f843e26d0154ea80ad949f1411
Nikos Zigopis <nzigopis@gmail.com>
2013-10-09 10:57:18 +0300

    Accepts JSON array AbsencesLog in PushAbsencesServlet

--- src/com/absences/PushAbsencesServlet.java
+++ src/com/absences/PushAbsencesServlet.java
@@ -2,20 +2,14 @@ package com.absences;
 
 import static com.googlecode.objectify.ObjectifyService.ofy;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.Collection;
-import java.util.List;
-
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.absences.models.AbsencesLog;
-import com.absences.models.SchoolClass;
-import com.absences.models.Student;
-import com.absences.models.User;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -28,30 +22,20 @@ public class PushAbsencesServlet extends HttpServlet
 		ObjectifyInitialize.Dummy();
 	}
 	
-	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException 
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException 
 	{
-		String s = "";
 		try
 		{
-			StringBuilder sb = new StringBuilder();
-		    BufferedReader br = req.getReader();
-		    String str;
-		    while( (str = br.readLine()) != null ){
-		        sb.append(str);
-		    }  
-		    
 			Gson gson = new Gson();
 			Type typeOfLog = new TypeToken<Collection<AbsencesLog>>(){}.getType();
 			Object log = gson.fromJson(req.getReader(), typeOfLog);
-			System.out.println(str);
+			System.out.println(log);
 		}
 		catch (Exception ex)
 		{
-			s = ex.getMessage();
+			System.out.println(ex.getMessage());
 		}
 		resp.setContentType("text/json; charset=utf-8");
-//		resp.setCharacterEncoding("UTF-8");
-//		resp.getWriter().println(s);
 		resp.getWriter().close();
 	}
 }
